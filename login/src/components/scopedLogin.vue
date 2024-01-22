11082676c541677921e5da8f589e5d3a42adcfb1
Maikol <maikoldepaula@gmail.com>
2024-01-22 19:47:15 -0300

    Save project

---
+++ login/src/components/scopedLogin.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="containerLogin">
+        <h1>Login</h1>
+
+        <div class="flex pl-5 mt-[40%] flex-col">
+            <h2 class="mb-10">E-mail  <input class="text-black w-72 bg-slate-300 outline-none rounded-sm" type="text"></h2>
+            <h2>Senha <input class="text-black w-72 bg-slate-300 outline-none rounded-sm" type="password"></h2>
+        </div>
+           <button class="btnLogin">Entrar</button>
+           <button class="flex justify-start pl-2 text-white">Cadastrar-se</button>
+       
+    </div>
+
+
+</template>
+<script setup>
+
+</script>
+<style scoped>
+h1{
+    @apply font-semibold text-slate-400 text-4xl mb-2 flex justify-center pb-1
+
+}
+h2{
+    @apply font-medium text-slate-200 
+
+}
+
+.btnLogin{
+
+    @apply mt-32 bg-gray-600 px-10 text-white ml-[35%] py-2 rounded-md hover:bg-blue-800 hover:-translate-y-1 
+
+}
+.containerLogin {
+
+    @apply h-[500px] w-[400px] border border-none rounded-lg bg-gray-700 bg-opacity-30 hover:bg-opacity-35
+
+
+}
+
+
+</style>
---
+++ login/src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+    <scopedLogin/>
+
+
+</template>
+<script setup>
+import scopedLogin from "@/components/scopedLogin.vue"
+
+
+
+</script>
+<style>
+    
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+    background: rgb(51,70,191);
+background: linear-gradient(90deg, rgba(51,70,191,0.3897934173669467) 0%, rgba(46,89,192,0.9108018207282913) 0%, rgba(46,137,192,0.9416141456582633) 11%, rgba(27,116,171,0.9051995798319328) 43%, rgba(27,71,171,0.9051995798319328) 89%);
+
+}
+
+}
+</style>
